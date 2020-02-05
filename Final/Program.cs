2a01cecaf2b98e52a79c4fed98f90b6e0e4dd0a8
Kaye Smith <50887040+kayesmith94@users.noreply.github.com>
2020-02-04 19:23:01 -0500

    test

---
+++ Final/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Much Candy? Let's get the conditions.");
+            Console.WriteLine("What's the temp like? enter 40, 50, 60 or 70");
+            double temp = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("And the weather? enter \"Clear\", \"Cloudy\", \"Raining\", or \"Full Moon\" ");
+            string weather = Console.ReadLine();
+
+            double candycount = HowMuchCandy(temp, weather);
+
+            Console.WriteLine("You should buy" + candycount + "pieces of candy.");
+        }
+         static double HowMuchCandy(int temp, string weather)
+        {
+            double kids = 250;
+            
+            switch(temp)
+            {
+                case 40:
+                    kids = kids - (kids * .05);
+                    break;
+                case 50:
+                    break;
+                case 60:
+                    kids = kids + (kids * .05);
+                    break;
+                case 70:
+                    kids = kids - (kids * .2);
+                    break;
+            }
+            
+            switch(weather)
+            {
+                case "Clear":
+                    kids = kids + (kids * .1);
+                    break;
+                case "Cloudy":
+                    break;
+                case "Raining":
+                    kids = kids - (kids * .25);
+                    break;
+                case "Full Moon":
+                    kids = kids + (kids * .25);
+                    break;
+            }
+
+            return kids*3;
+        }
+    }
+}
